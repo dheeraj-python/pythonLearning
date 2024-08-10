0388f2c7ead34d3302e25222b0a9e062a88d9716
DheerajPC <dheerajofficial27@gmail.com>
2024-08-10 14:03:14 +0530

    Task 1 completed

---
+++ lab002.py
@@ -0,0 +1,11 @@
+# Task1 - Take a user input (name, age, roll_number & phone_number) & print into console
+name = input("Enter Your Name")
+age = input("Enter your age")
+roll_number = input("Enter your roll number")
+phone_number = input("Enter your phone number")
+
+# Now here print the user inputs
+print("Your Name is", name)
+print("Your age is", age)
+print("Your roll number is", roll_number)
+print("Your phone number is", phone_number)
