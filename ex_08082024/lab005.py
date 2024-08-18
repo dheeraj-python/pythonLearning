2f8da8ee9f66e49588b237a755b1ef1736852cbd
DheerajPC <dheerajofficial27@gmail.com>
2024-08-18 18:27:09 +0530

    Task 5 complted

---
+++ ex_08082024/lab005.py
@@ -0,0 +1,10 @@
+# Task5
+
+first_number = int(input("Enter the first number:"))
+second_number = int(input("Enter the second number:"))
+if first_number > second_number:
+    print("First number is greater")
+elif first_number < second_number:
+    print("Second number is greater")
+else:
+    print("First number is equals to second number")
