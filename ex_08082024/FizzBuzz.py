6465a725a9db94db2001bdc078a4ea1b1d97b4fd
DheerajPC <dheerajofficial27@gmail.com>
2024-08-25 19:32:30 +0530

    Task 8 complted

---
+++ ex_08082024/FizzBuzz.py
@@ -0,0 +1,12 @@
+# FizzBUzz Program
+
+#num = int(input("Enter the number:"))
+for num in range(1,100):
+    if num % 3 == 0:
+        print("Fizz")
+    elif num % 5 == 0:
+        print("Buzz")
+    else:
+        print("Fizz Buzz")
+
+
